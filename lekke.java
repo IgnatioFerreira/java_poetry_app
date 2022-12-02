d3554b18aabd1dacd848271c0b428354ff74f850
Ignatio Ferreira <ignatio.ferreira@sybrin.com>
2022-12-02 21:19:46 +0200

    yessir

--- lekke.java
+++ lekke.java
@@ -10,10 +10,66 @@ import java.util.Scanner;
 public class lekke {
 
     public static void main(String[] args) throws IOException {
-        getListOfPoemLinksFromAuthor("https://mypoeticside.com/poets/a-r-ammons-poems");
+        browseByLetterAndGetPoetLinks("https://mypoeticside.com/z-browse");
+    }
+
+    public static void browseByLetterAndGetPoetLinks(String url) throws IOException {
+        new File("C:\\Users\\IFerreira\\Desktop\\java_poetry_app\\poems\\"
+                + url.substring(url.lastIndexOf("/"), url.lastIndexOf("-")))
+                .mkdirs();
+
+        PrintStream out = new PrintStream(new FileOutputStream("output.txt"));
+        System.setOut(out);
+
+        String content = null;
+        URLConnection connection = null;
+        try {
+            connection = new URL(url).openConnection();
+            Scanner scanner = new Scanner(connection.getInputStream());
+            scanner.useDelimiter("\\Z");
+            content = scanner.next();
+            scanner.close();
+        } catch (Exception ex) {
+            ex.printStackTrace();
+        }
+        System.out.println(content);
+
+        PrintStream out2 = new PrintStream(new FileOutputStream("processed.txt"));
+        System.setOut(out2);
+
+        File file = new File("C:\\Users\\IFerreira\\Desktop\\java_poetry_app\\output.txt");
+        Scanner sc = new Scanner(file);
+        String line = "";
+        while (sc.hasNextLine()) {
+            line = sc.nextLine();
+
+            if (line.matches(".+list-poems.+")) {
+
+                while (line.indexOf("href") > 0) {
+
+    
+                    line = line.substring(line.indexOf("href=\"https://mypoeticside.com/poets/") + 6);
+                    String poetLink = line.substring(0, line.indexOf("\">"));
+                    System.out.println(poetLink);
+    
+                }
+
+
+
+            }
+
+        }
+        sc.close();
+
     }
 
     public static void getListOfPoemLinksFromAuthor(String url) throws IOException {
+
+        // new File("C:\\Users\\IFerreira\\Desktop\\java_poetry_app\\poems\\" + letter)
+        // .mkdirs();
+
+        new File("C:\\Users\\IFerreira\\Desktop\\java_poetry_app\\poems\\" + url.substring(url.lastIndexOf("/")))
+                .mkdirs();
 
         PrintStream out = new PrintStream(new FileOutputStream("output.txt"));
         System.setOut(out);
@@ -35,7 +91,7 @@ public class lekke {
         PrintStream out2 = new PrintStream(new FileOutputStream("processed.txt"));
         System.setOut(out2);
 
-        File file = new File("C:\\Users\\app3l\\Desktop\\java_poetry_app\\output.txt");
+        File file = new File("C:\\Users\\IFerreira\\Desktop\\java_poetry_app\\output.txt");
         Scanner sc = new Scanner(file);
         String line = "";
         boolean inrange = false;
@@ -58,20 +114,22 @@ public class lekke {
         PrintStream out3 = new PrintStream(new FileOutputStream("list_of_poem_links.txt"));
         System.setOut(out3);
 
-        file = new File("C:\\Users\\app3l\\Desktop\\java_poetry_app\\processed.txt");
+        file = new File("C:\\Users\\IFerreira\\Desktop\\java_poetry_app\\processed.txt");
         sc = new Scanner(file);
         line = "";
 
         while (sc.hasNextLine()) {
             line = sc.nextLine();
 
-            int index = 0;
-            line = line.substring(line.indexOf("href") + 6);
             while (line.indexOf("href") > 0) {
-                String poemlink = line.substring(index, line.indexOf("\">"));
+                System.setOut(out3);
+
                 line = line.substring(line.indexOf("href") + 6);
-                System.out.println("https:" + poemlink);
-                getPoemAndPrintToFile("https:" + poemlink);
+                String poemlink = line.substring(0, line.indexOf("\">"));
+                System.out.println(line);
+                System.out.println(poemlink);
+
+                getPoemAndPrintToFile("https:" + poemlink, url.substring(url.lastIndexOf("/")));
             }
 
         }
@@ -79,7 +137,7 @@ public class lekke {
 
     }
 
-    public static void getPoemAndPrintToFile(String url) throws IOException {
+    public static void getPoemAndPrintToFile(String url, String dir) throws IOException {
         // https://stackoverflow.com/questions/1994255/how-to-write-console-output-to-a-txt-file
         PrintStream out = new PrintStream(new FileOutputStream("output.txt"));
         System.setOut(out);
@@ -99,11 +157,11 @@ public class lekke {
         System.out.println(content);
 
         String filename = url.substring(url.lastIndexOf("/"));
-        PrintStream out2 = new PrintStream(new FileOutputStream("poems\\" + filename + ".txt"));
+        PrintStream out2 = new PrintStream(new FileOutputStream("poems\\" + dir + "\\" + filename + ".txt"));
         System.setOut(out2);
 
         // https://www.geeksforgeeks.org/different-ways-reading-text-file-java/
-        File file = new File("C:\\Users\\app3l\\Desktop\\java_poetry_app\\output.txt");
+        File file = new File("C:\\Users\\IFerreira\\Desktop\\java_poetry_app\\output.txt");
         Scanner sc = new Scanner(file);
         String line = "";
         boolean inrange = false;
